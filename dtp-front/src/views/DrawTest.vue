8d02a6fe6116009b8260127455a3555db94c428c
Lisa Gernez <assiette3000@gmail.com>
2021-01-18 09:48:26 +0100

    begin work on draw area

---
+++ dtp-front/src/views/DrawTest.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <CanvasDraw :width="640" :height="480" :outputName="'example'"/>
+  </div>
+</template>
+<script>
+import CanvasDraw from '@/components/draw/CanvasDraw.vue';
+export default {
+  components: {CanvasDraw},
+}
+</script>
