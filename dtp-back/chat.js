89a27f47d7e10caad3a707cb1bf2b4c1fe76445b
Nils <nils.richard@free.fr>
2021-01-27 17:31:57 +0100

    Sending username of message sender

--- dtp-back/chat.js
+++ dtp-back/chat.js
@@ -1,26 +1,29 @@
-const io = require('./server')
-const log = require('./log')
+const io = require("./server")
+const log = require("./log")
 
 // Chat
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
     const username = socket.decoded_token.username
-    log.debug('User: ' + username + ' is connected')
-    socket.on('disconnect', () => {
-        log.debug('User: ' + username + ' disconnected')
+    log.debug("User: " + username + " is connected")
+    socket.on("disconnect", () => {
+        log.debug("User: " + username + " disconnected")
     })
 
-    socket.on('connectMeTo', (chatRoom) => {
-        log.debug('Connecting '+username+' to chat room '+chatRoom)
+    socket.on("connectMeTo", (chatRoom) => {
+        log.debug("Connecting " + username + " to chat room " + chatRoom)
         socket.join(chatRoom)
-        io.to(chatRoom).emit('game', "newUserInRoom")
+        io.to(chatRoom).emit("game", "newUserInRoom")
 
-        socket.on('chat', (msg)=>{
-            log.debug('('+chatRoom+') (' + username + ') : ' + msg)
-            io.to(chatRoom).emit('chat', msg)
+        socket.on("chat", (msg) => {
+            log.debug("(" + chatRoom + ") (" + username + ") : " + msg)
+            io.to(chatRoom).emit("chat", {
+                username: username,
+                msg: msg
+            })
         })
-        socket.on('draw', (msg)=>{
-            log.debug('('+chatRoom+') (' + username + ') [draw] : ' + msg)
-            io.to(chatRoom).emit('draw', msg)
+        socket.on("draw", (msg) => {
+            log.debug("(" + chatRoom + ") (" + username + ") [draw] : " + msg)
+            io.to(chatRoom).emit("draw", msg)
         })
     })
 })
