ee0590ca91e00dfd5e79303a84ae0c99c76563a3
BEUCHER Maxime <maxime.beucher@etudiant.univ-rennes1.fr>
2021-02-03 15:01:02 +0100

    Ajout solved générateur de mot synchrone

--- dtp-back/model/wordpicker.js
+++ dtp-back/model/wordpicker.js
@@ -8,32 +8,11 @@ class RandLine {
     }
 
     lines(cb, count=1) {
-        let self;
-        self = this;
-
-        let stats = fs.stat(self.filename, (err, stats) => {
-            let chunk_range, options, stream;
-
-            chunk_range = stats.size - 1024;
-            options = {};
-            if (chunk_range > 0) {
-                options.start = Math.round(Math.random() * chunk_range);
-                options.end = options.start + 1024;
-            }
-            stream = fs.createReadStream(self.filename, options);
-
-            while (count-- > 0) {
-                stream.on('data', function (d) {
-                    let ar = d.toString().split(/\r?\n/);
-                    //ar.splice(0, 1);
-                    //ar.splice(ar.length - 1, 1);
-                    stream.destroy();
-                    self.semaphore--;
-                    let word = ar[Math.round(Math.random() *  (ar.length - 1))];
-                    typeof cb === "function" ? cb(word) : void 0;
-                });
-            }
-        });
+        let data = fs.readFileSync(this.filename, "utf8");
+        let ls = data.split('\r\n');
+        while (count-- > 0) {
+            cb(ls[Math.floor(Math.random() * ls.length)]);
+        }
     }
 }
 
