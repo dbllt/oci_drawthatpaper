bb9098264ae433ad1db5ecac037d25a130918be6
BEUCHER Maxime <maxime.beucher@etudiant.univ-rennes1.fr>
2021-02-03 17:27:27 +0100

    Ajout spécification encodage lecture fichier

--- dtp-back/model/wordpicker.js
+++ dtp-back/model/wordpicker.js
@@ -2,18 +2,30 @@ const readline = require('readline');
 const fs = require('fs');
 
 class RandLine {
-    constructor(filename) {
+    constructor(filename, encoding="latin1") {
         this.filename = filename;
-        this.semaphore = 0;
+        this.encoding = encoding;
     }
 
     lines(cb, count=1) {
-        let data = fs.readFileSync(this.filename, "utf8");
+        let data = fs.readFileSync(this.filename, this.encoding);
         let ls = data.split('\r\n');
         while (count-- > 0) {
             cb(ls[Math.floor(Math.random() * ls.length)]);
         }
     }
 }
+//throw new Error("TODO");
+let generator = new RandLine('wordbank/français.txt');
+currentWords = [];
 
+let callback = (word) => {
+    if (word.length < 2) {
+        generator.lines(callback, 1);
+        return ;
+    }
+    currentWords.push(word);
+};
+generator.lines(callback, 3);
+console.log("", currentWords)
 module.exports = RandLine;
