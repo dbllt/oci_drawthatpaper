644edf0092a06d6c4a58e584e760c9ad60ed029f
Nils <nils.richard@free.fr>
2021-02-04 13:59:33 +0100

    Improves code readability
    Documentation
    RoomManager and GameManager have their own file and are Singletons now

---
+++ dtp-back/model/game_manager.js
@@ -0,0 +1,39 @@
+const {
+    Game
+} = require("../game")
+const log = require('../log')
+
+class GameManager {
+    constructor() {
+        if (GameManager.exists) {
+            return GameManager.instance
+        }
+        // Instance don't exist, initialisation of GameManager
+        this._games = {}
+
+        GameManager.instance = this
+        GameManager.exists = true
+        return this
+    }
+
+    /**
+     * Creates a game 
+     * @param {*} roomId 
+     */
+    createGame(roomId) {
+        this._games[roomId] = new Game(roomId, 3, 20)
+    }
+
+    /**
+     * Sends the event to the running game
+     * @param {*} event 
+     * @param {*} roomId 
+     */
+    processEvent(event, roomId) {
+        if(!this._games[roomId]) return log.error("processEvent: Game don't exist")
+        this._games[roomId].processEvent(event)
+    }
+
+}
+
+module.exports.GameManager = GameManager
