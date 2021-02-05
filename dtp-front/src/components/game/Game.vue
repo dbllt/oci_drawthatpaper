b4c194987ad0e5a88f871d3bcb6334acdd7d42a8
BEUCHER Maxime <maxime.beucher@etudiant.univ-rennes1.fr>
2021-02-05 01:40:51 +0100

    Ajout overlays

--- dtp-front/src/components/game/Game.vue
+++ dtp-front/src/components/game/Game.vue
@@ -1,51 +1,77 @@
 <template>
   <div class="w3-container w3-center">
-    <h1>Draw That Paper</h1>
-    <h2>{{ this.gameState }}</h2>
+    <!--h1>Draw That Paper</h1-->
+    <!--h2>{{ this.gameState }}</h2-->
     <div>
       <div class="wordToDraw">
         <div class="vertical-center">
           <template v-if="currentWord">
-            <h3>Your word :</h3>
-            <br />
             <h2>
-              <b>{{ currentWord }}</b>
+              Your word : <b>{{ currentWord }}</b>
             </h2>
           </template>
           <br />
-          <h3><Timer ref="timer" /></h3>
         </div>
       </div>
-      <CanvasDraw
-        v-bind:class="{ disabled: this.canvaDisabled }"
-        ref="canva"
-        class="canvas"
-        :width="480"
-        :height="480"
-        :brushSize="4"
-        :outputName="'example'"
-      />
-      <div class="scores">
-        <h4>Scores :</h4>
 
-        <table>
-          <tr v-for="item in this.participants" :key="item.id">
-            <th>{{ item.username }}</th>
-            <th>{{ item.score }}</th>
-          </tr>
-        </table>
+      <div class="parent">
+        <CanvasDraw
+          v-bind:class="{ disabled: this.canvaDisabled }"
+          ref="canva"
+          class="canvas"
+          :width="480"
+          :height="480"
+          :brushSize="4"
+          :outputName="'example'"
+        />
+        <h3 class="childright"><Timer ref="timer" /></h3>
+
+        <div class="scores child">
+          <table>
+            <tr>
+              <th>
+                <h4>Scores :</h4>
+              </th>
+            </tr>
+            <tr v-for="item in this.participants" :key="item.id">
+              <th>{{ item.username }}</th>
+              <th>{{ item.score }}</th>
+            </tr>
+          </table>
+        </div>
+        <div class="clearfix"></div>
       </div>
-      <div class="clearfix"></div>
     </div>
-    <br />
-    <h4 v-if="currentWords.length">Pick a word:</h4>
-    <div class="wordList" v-for="item in currentWords" :key="item">
-      <div class="w3-button myButton w3-margin  w3-large w3-theme-yellow" v-on:click="pickWord(item)">
-        {{ item }}
+
+    <div class="overlay" v-if="currentWords.length === 0 && this.gameState === 'Picking'">
+      <h1>Wait picker...</h1>
+    </div>
+    
+    <div class="overlay" v-if="currentWords.length">
+      <div class="centered">
+        <h4 v-if="currentWords.length">Pick a word:</h4>
+        <div class="wordList" v-for="item in currentWords" :key="item">
+          <div class="w3-button myButton w3-margin  w3-large w3-theme-yellow" v-on:click="pickWord(item)">
+            {{ item }}
+          </div>
+        </div>
       </div>
     </div>
+
+    <div class="overlay" v-if="this.gameState === 'Ended'">
+      <div class="centered">
+        <h2>Game is over :)</h2>
+        <div class="scores centered">
+            <ul v-for="item in this.participants" :key="item.id">
+              <li>{{ item.username }}: {{ item.score }}</li>
+            </ul>
+        </div>
+      </div>
+    </div>
+
     <div class="clearfix"></div>
     <br />
+
     <carousel ref="carousel" :buttons="true"></carousel>
     <div class="w3-button myButton w3-margin  w3-large w3-theme-red" v-on:click="leave">Leave</div>
   </div>
@@ -62,6 +88,7 @@ export default {
   components: { CanvasDraw, Timer, carousel },
   data: function() {
     return {
+      activeModal: true,
       currentWords: [],
       currentWord: "",
       gameState: "",
@@ -138,7 +165,7 @@ export default {
     onRoundTime(roundTime) {
       this.roundTime = roundTime;
     },
-  },
+},
   created() {
     this.$connection.$on(this.$network_events.PickWord, this.onPickWord);
     this.$connection.$on(
@@ -168,3 +195,43 @@ export default {
 };
 </script>
 
+<style scoped>
+.overlay {
+  position: fixed; /* Sit on top of the page content */
+  width: 100%; /* Full width (cover the whole page) */
+  height: 100%; /* Full height (cover the whole page) */
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.5); /* Black background with opacity */
+  z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
+  cursor: pointer; /* Add a pointer on hover */
+}
+.child {
+  /* ... */
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding-left: 10px;
+  pointer-events: none;
+}
+
+.childright {
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding-right:10px;
+  pointer-events: none;
+}
+.centered {
+  width:100%;
+  margin: auto;
+}
+.parent {
+  position: relative;
+}
+.scores {
+  font-size: 10px;
+}
+</style>
